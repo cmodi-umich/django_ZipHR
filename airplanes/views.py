53672b8a7b246e2d7d12aa63cbacf8145dca6aed
cmodi <cmodi@umich.edu>
2020-03-20 16:42:19 -0400

    Added Simple Frontend

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from .api import airplane_list, airplane_detail
+from .models import Airplane
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome to ZipAirlines!")
+    airplanes_list = Airplane.objects.all()
+    context = {'airplanes_list': airplanes_list}
+    return render(request, 'airplanes/index.html', context)
